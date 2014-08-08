21d8e094d911b512dc88a72b7bbb9e18942cd485
Alex <alex@mt-alexmbp.internal.mindtribe.com>
2014-08-07 18:29:59 -0700

    modified tekCaptureWeb to capture all 4 channels and get a screen capture and save these to a local directory. added the TektronixMS)4104BL to mtest.py and included the get_screen_capture functionality from tekCaptureWeb. Also got the same function working in case you are connected via usb and want to save to a thumb drive.

--- mtest.py
+++ mtest.py
@@ -3,6 +3,11 @@ import os
 import serial
 import visa
 from sys import platform
+import mechanize
+import time
+import csv
+import shutil
+from BeautifulSoup import BeautifulSoup as soup
 
 #globals
 INSTRUMENT_DIRECTORY = './instruments'
@@ -25,6 +30,10 @@ class Instrument(object):
         instrumentFileDict = json.load(instrumentFile)
         self.parametersDict = instrumentFileDict['parameters']
         self.terminationCharacters = str(self.parametersDict['terminationCharacters'])
+        if self.parametersDict['usbAddress'] == 'None':
+            self.usbAddress = None
+        else:
+            self.usbAddress = self.parametersDict['usbAddress']
         if self.parametersDict['ipAddress'] == 'None':
             self.ipAddress = None
         else:
@@ -67,6 +76,8 @@ class Instrument(object):
             return self.handle.read(SERIAL_READ_SIZE)
         elif self.communicationProtocol is 'ethernet':
             return self.handle.ask(self.get_command_string(commandName) % parametersTuple)
+        elif self.communicationProtocol is 'usb':
+            return self.handle.ask(self.get_command_string(commandName) % parametersTuple)
 
 
     def connect(self):
@@ -103,10 +114,21 @@ class Instrument(object):
                 #set termination characters so instrument knows when to stop listening and execute a command
                 self.handle.term_chars = self.terminationCharacters
 
+        elif self.communicationProtocol is 'usb':
+            #check if device can connect via usb
+            if self.usbAddress is None:
+                print 'Error. This instrument has not been configured to connect via usb. Please specify the instrument\'s usb address in its corresponding JSON file.'
+            else:
+                self.handle = visa.instrument(self.usbAddress)
+                #set termination characters so instrument knows when to stop listening and execute a command
+                self.handle.term_chars = self.terminationCharacters
+
     def disconnect(self):
         if self.communicationProtocol is 'serial':
             self.handle.close()
         elif self.communicationProtocol is 'ethernet':
+            self.handle.close()
+        elif self.communicationProtocol is 'usb':
             self.handle.close()
 
     def get_id(self):
@@ -191,6 +213,11 @@ class ElectronicLoad(Instrument):
     def get_power(self):
         return self.send_command('get_power')
 
+#oscilloscope class
+class Oscilloscope(Instrument):
+    def get_screen_capture(self):
+        self.send_command('get_screen_capture')
+
 #specific instrument classes
 #dc power supplies
 class AgilentE3631A(DCPowerSupply):
@@ -211,3 +238,131 @@ class AgilentE3633A(DCPowerSupply):
 class Agilent6060B(ElectronicLoad):
     def get_error(self):
         return self.send_command('get_error')
+
+#oscilloscopes
+class TektronixMSO4104BL(Oscilloscope):
+    def get_screen_capture(self):
+        if (self.communicationProtocol is 'usb') or (self.communicationProtocol is 'serial'):
+            #globals
+            WAIT_TIME = 0.5
+            OUTPUT_WAVEFORMS_FILENAME = 'waveforms'
+            OUTPUT_SCREENCAPTURE_FILENAME = 'screenCapture'
+            OUTPUT_WAVEFORMS_FILE_EXTENSION = '.csv'
+            OUTPUT_SCREENCAPTURE_FILE_EXTENSION = '.png'
+
+            #send stop command to freeze waveforms
+            self.handle.write("ACQuire:STATE STOP")
+
+            #create unique timestamped filename
+            timestr = time.strftime('%Y%m%d-%H%M%S')
+            #create directory to save screen captures and final csv file with all channel waveforms
+            outputDir = os.path.join('E:', timestr + '-scope-capture')
+            self.handle.write("FILESystem:MKDir \"" + outputDir + "\"")
+
+            #save waveforms
+            waveformsFilename = os.path.join(outputDir, OUTPUT_WAVEFORMS_FILENAME + OUTPUT_WAVEFORMS_FILE_EXTENSION)
+            self.handle.write("SAVE:WAVEFORM ALL,\"" + waveformsFilename + "\"")
+            while '1' in self.handle.ask("BUSY?"):
+                    time.sleep(WAIT_TIME)
+
+            #save screencapture
+            screenCaptureFilename = os.path.join(outputDir, OUTPUT_SCREENCAPTURE_FILENAME + OUTPUT_SCREENCAPTURE_FILE_EXTENSION)
+            self.handle.write("SAVE:IMAGE \"" + screenCaptureFilename + "\"")
+            while '1' in self.handle.ask("BUSY?"):
+                    time.sleep(WAIT_TIME)
+
+            #send run command to unfreeze waveforms
+            self.handle.write("ACQuire:STATE RUN")
+
+        elif self.communicationProtocol is 'ethernet':
+            #globals
+            CHANNELS = ['ch1', 'ch2', 'ch3', 'ch4']
+            OUTPUT_FORMAT = 'spreadsheet'
+            OUTPUT_WAVEFORMS_FILENAME = 'waveforms'
+            OUTPUT_SCREENCAPTURE_FILENAME = 'screenCapture'
+            OUTPUT_WAVEFORMS_FILE_EXTENSION = '.csv'
+            OUTPUT_SCREENCAPTURE_FILE_EXTENSION = '.png'
+            ROW_OFFSET = 19
+
+            #send stop command to freeze waveforms
+            self.handle.write("ACQuire:STATE STOP")
+
+            #create unique timestamped filename
+            timestr = time.strftime('%Y%m%d-%H%M%S')
+            #create temporary directory for individual channel waveform csv files
+            tmpDir = timestr + '-scope-capture-tmp'
+            os.mkdir(tmpDir)
+            #create directory to save screen captures and final csv file with all channel waveforms
+            outputDir = timestr + '-scope-capture'
+            os.mkdir(outputDir)
+
+            #save waveform to csv for each channel
+            for channel in CHANNELS:
+                br = mechanize.Browser()
+                br.open('http://192.168.171.84:81/data/mso_data4.html')
+                br.select_form(name='firstForm')
+                channelControl = br.find_control('command')
+                channelControl.value = ['select:control ' + channel]
+                fileFormatControl = br.find_control('command1')
+                fileFormatControl.value = ['save:waveform:fileformat ' + OUTPUT_FORMAT]
+                response = br.submit()
+                outputFile = open(os.path.join(tmpDir, channel + OUTPUT_WAVEFORMS_FILE_EXTENSION), "wb")
+                outputFile.write(response.read())
+                outputFile.close()
+
+            #combine all csv channel waveform files into one
+            csvDict = {}
+            for channel in CHANNELS:
+                f = open(os.path.join(tmpDir, channel + OUTPUT_WAVEFORMS_FILE_EXTENSION), 'rb')
+                reader = csv.reader(f)
+                rows = []
+                for rowNumber, row in enumerate(reader):
+                    if rowNumber < ROW_OFFSET:
+                        rows.append(row)
+                    else:
+                        if len(row) > 0:
+                            rows.append(row)
+                csvDict[channel] = rows
+                f.close()
+
+            #check to make sure that all csv files had the same number of rows
+            numRows = len(csvDict[CHANNELS[0]])
+            for key in csvDict:
+                if len(csvDict[key]) != numRows:
+                    raise Exception('Error: The number of rows in the csv files for individual channels are not equal.')
+
+            waveformsFilename = os.path.join(outputDir, OUTPUT_WAVEFORMS_FILENAME + OUTPUT_WAVEFORMS_FILE_EXTENSION)
+            fout = open(waveformsFilename, 'wb')
+            writer = csv.writer(fout)
+            for row in range(numRows):
+                rowList = []
+                for numChannel, channel in enumerate(CHANNELS):
+                    if row < ROW_OFFSET:
+                        if numChannel is 0:
+                            rowList += csvDict[channel][row]
+                    else:
+                        if numChannel is 0:
+                            rowList += csvDict[channel][row]
+                        else:
+                            rowList += [csvDict[channel][row][1]]
+                writer.writerow(rowList)
+
+            #delete temporary directory
+            shutil.rmtree(tmpDir)
+
+            #get screencapture
+            br = mechanize.Browser()
+            html = br.open('http://192.168.171.84:81/control/control.html')
+            bsoup = soup(html)
+            image_tags = bsoup.findAll('img', {'id': 'thescreen'})
+            image = image_tags[0]
+            screenCaptureFilename = os.path.join(outputDir, OUTPUT_SCREENCAPTURE_FILENAME + OUTPUT_SCREENCAPTURE_FILE_EXTENSION)
+            data = br.open(image['src']).read()
+            br.back()
+            save = open(screenCaptureFilename, 'wb')
+            save.write(data)
+            save.close()
+
+            #send run command to unfreeze waveforms
+            self.handle.write("ACQuire:STATE RUN")
+
--- tekCaptureWeb.py
+++ tekCaptureWeb.py
@@ -1,17 +1,99 @@
 import mechanize
+import visa
+import os
+import time
+import csv
+import shutil
+from BeautifulSoup import BeautifulSoup as soup
 
 #globals
-CHANNEL = 'ch1'
+CHANNELS = ['ch1', 'ch2', 'ch3', 'ch4']
 OUTPUT_FORMAT = 'spreadsheet'
-OUTPUT_FILENAME = 'ch1.csv'
+OUTPUT_WAVEFORMS_FILENAME = 'waveforms'
+OUTPUT_FILE_EXTENSION = '.csv'
+ROW_OFFSET = 19
 
+#set up connection to scope over LAN
+scope = visa.instrument("TCPIP::192.168.171.84")
+#send stop command to freeze waveforms
+scope.write("ACQuire:STATE STOP")
+
+#create unique timestamped filename
+timestr = time.strftime("%Y%m%d-%H%M%S")
+#create temporary directory for individual channel waveform csv files
+tmpDir = timestr + '-scope-capture-tmp'
+os.mkdir(tmpDir)
+#create directory to save screen captures and final csv file with all channel waveforms
+outputDir = timestr + '-scope-capture'
+os.mkdir(outputDir)
+
+#save waveform to csv for each channel
+for channel in CHANNELS:
+	br = mechanize.Browser()
+	br.open('http://192.168.171.84:81/data/mso_data4.html')
+	br.select_form(name='firstForm')
+	channelControl = br.find_control('command')
+	channelControl.value = ['select:control ' + channel]
+	fileFormatControl = br.find_control('command1')
+	fileFormatControl.value = ['save:waveform:fileformat ' + OUTPUT_FORMAT]
+	response = br.submit()
+	outputFile = open(os.path.join(tmpDir, channel + OUTPUT_FILE_EXTENSION), "wb")
+	outputFile.write(response.read())
+	outputFile.close()
+
+#combine all csv channel waveform files into one
+csvDict = {}
+for channel in CHANNELS:
+	f = open(os.path.join(tmpDir, channel + OUTPUT_FILE_EXTENSION), 'rb')
+	reader = csv.reader(f)
+	rows = []
+	for rowNumber, row in enumerate(reader):
+		if rowNumber < ROW_OFFSET:
+			rows.append(row)
+		else:
+			if len(row) > 0:
+				rows.append(row)
+	csvDict[channel] = rows
+	f.close()
+
+#check to make sure that all csv files had the same number of rows
+numRows = len(csvDict[CHANNELS[0]])
+for key in csvDict:
+	if len(csvDict[key]) != numRows:
+		raise Exception('Error: The number of rows in the csv files for individual channels are not equal.')
+
+fout = open(os.path.join(outputDir, OUTPUT_WAVEFORMS_FILENAME + OUTPUT_FILE_EXTENSION), 'wb')
+writer = csv.writer(fout)
+for row in range(numRows):
+	rowList = []
+	for numChannel, channel in enumerate(CHANNELS):
+		if row < ROW_OFFSET:
+			if numChannel is 0:
+				rowList += csvDict[channel][row]
+		else:
+			if numChannel is 0:
+				rowList += csvDict[channel][row]
+			else:
+				rowList += [csvDict[channel][row][1]]
+	writer.writerow(rowList)
+
+#delete temporary directory
+shutil.rmtree(tmpDir)
+
+#get screencapture
 br = mechanize.Browser()
-br.open('http://192.168.171.84:81/data/mso_data4.html')
-br.select_form(name='firstForm')
-channelControl = br.find_control('command')
-channelControl.value = ['select:control ' + CHANNEL]
-fileFormatControl = br.find_control('command1')
-fileFormatControl.value = ['save:waveform:fileformat ' + OUTPUT_FORMAT]
-response = br.submit()
-outputFile = open(OUTPUT_FILENAME, "wb")
-outputFile.write(response.read())
+html = br.open('http://192.168.171.84:81/control/control.html')
+bsoup = soup(html)
+image_tags = bsoup.findAll('img', {'id': 'thescreen'})
+image = image_tags[0]
+# filename = image['src'].lstrip('http://')
+# filename = os.path.join(outputDir, filename.replace('/', '_'))
+screenCaptureFilename = os.path.join(outputDir, 'screenCapture.png')
+data = br.open(image['src']).read()
+br.back()
+save = open(screenCaptureFilename, 'wb')
+save.write(data)
+save.close()
+
+#send run command to unfreeze waveforms
+scope.write("ACQuire:STATE RUN")
